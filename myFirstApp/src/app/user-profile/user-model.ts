7f080165b8c9d67adab853ad316bf820ff84e121
Omar MELLOULCHI <omar.melloulchi@greenwishes.fr>
2023-08-13 13:04:10 +0200

    added user class

---
+++ myFirstApp/src/app/user-profile/user-model.ts
@@ -0,0 +1,22 @@
+
+export class User {
+    name: string;
+    firstName: string;
+    age: number;
+    quote: string;
+    photo: string;
+    showAge: boolean;
+
+    constructor(name: string, firstName: string, age: number, photo: string, quote: string = '', showAge: boolean = true) {
+        this.name = name;
+        this.firstName = firstName;
+        this.age = age;
+        this.photo = photo;
+        this.quote = quote;
+        this.showAge = showAge;
+    }
+
+    toggleShowAge(): void {
+        this.showAge = !this.showAge;
+    }
+}
--- myFirstApp/src/app/user-profile/user-profile.component.ts
+++ myFirstApp/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user-model';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,16 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  user = {
-    name : 'Doe',
-    firstName : 'John',
-    age : 25,
-    quote : '',
-    photo : 'https://randomuser.me/api/portraits/lego/2.jpg',
-    showAge : true
-  };
+  user: User
 
-  constructor() { }
+  constructor() { 
+    this.user = new User('Doe', 'John', 25, 'https://randomuser.me/api/portraits/lego/2.jpg');
+
+  }
 
   ngOnInit(): void {
   }
